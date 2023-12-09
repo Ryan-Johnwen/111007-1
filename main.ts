b4acb0f97e4382ab5bb5f17abecc7934f74d9869
Ryan-Johnwen <153349240+Ryan-Johnwen@users.noreply.github.com>
2023-12-09 16:24:31 +0800

    e

--- main.ts
+++ main.ts
@@ -1,10 +1,24 @@
-let y = 0
-let x = 0
-led.plot(x, y)
-for (let index = 0; index <= 4; index++) {
+function light2 (num: number, light22: boolean) {
+    if (num < 6) {
+        x = num - 1
+        y = 0
+    } else if (num >= 6 && num < 9) {
+        x = 4
+        y = num - 5
+    } else if (num >= 9 && num < 14) {
+        x = 13 - num
+        y = 4
+    } else if (num >= 14 && num <= 16) {
+        x = 0
+        y = 17 - num
+    }
     led.plot(x, y)
 }
-led.plot(x, y + 1)
-for (let x = 0; x <= 4; x++) {
-    led.plot(y, x - 1)
-}
+let y = 0
+let x = 0
+let index = 0
+basic.forever(function () {
+    index += 1
+    light2(index, true)
+    basic.pause(100)
+})
